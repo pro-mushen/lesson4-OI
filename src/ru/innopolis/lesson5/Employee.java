ed179b3dac5ad6dd443cc473cbab7acf46db592f
Mushen <mirmushen@gmail.com>
2018-08-30 18:54:17 +0300

    Add Externalizable

--- src/ru/innopolis/lesson5/Employee.java
+++ src/ru/innopolis/lesson5/Employee.java
@@ -1,9 +1,9 @@
 package ru.innopolis.lesson5;
 
-import java.io.Serializable;
+import java.io.*;
 import java.util.Objects;
 
-public class Employee implements Serializable {
+public class Employee implements Externalizable {
     private String name;
     private int age;
     private double salary;
@@ -18,6 +18,22 @@ public class Employee implements Serializable {
         this.age = age;
         this.salary = salary;
         this.job = job;
+    }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeUTF(name);
+        out.writeInt(age);
+        out.writeDouble(salary);
+        out.writeUTF(job);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        name = in.readUTF();
+        age = in.readInt();
+        salary = in.readDouble();
+        job = in.readUTF();
     }
 
     public String getName() {
@@ -76,4 +92,6 @@ public class Employee implements Serializable {
     public int hashCode() {
         return Objects.hash(name, age, salary, job);
     }
+
+
 }
